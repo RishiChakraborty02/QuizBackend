e4f7d582f331aa10725a56d5950f206b335d0634
Rishi <rishichakraborty02@gmail.com>
2024-09-23 01:15:21 +0530

    added github actions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/routes.js";
+import dbPool from "./db/connection.js";
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use("/api/v1", authRouter);
 
 app.listen(3000, () => {
+  dbPool.connect().then(console.log("Database connection successfull"));
   console.log("Server is running on port 3000");
 });
--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
-import pg from 'pg';
+import pg from "pg";
 
 const pool = new pg.Pool({
-    host: 'localhost',
-    database:"quiz",
-
-})
-
+  // host: 'localhost',
+  // database:"quiz",
+  connectionString:
+    "postgres://postgres.xscbdawydtmromqwbkgk:quizquest@123@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
+});
 
 export default pool;
