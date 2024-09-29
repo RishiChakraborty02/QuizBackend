ea8112883b9767d1883a8023951501731d65e19b
Rishi <rishichakraborty02@gmail.com>
2024-09-29 22:24:23 +0530

    changed nginx

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 
-app.use("/api/v1", authRouter);
+// app.use("/api/v1", authRouter);
 
 app.listen(3000, () => {
   // dbPool.connect().then(console.log("Database connection successfull"));
