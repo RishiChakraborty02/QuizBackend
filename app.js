f748da077949c2ad77cf8891f250bea3a3db5d49
Rishi <rishichakraborty02@gmail.com>
2024-09-29 22:04:06 +0530

    changed pem

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import authRouter from "./routes/routes.js";
-import dbPool from "./db/connection.js";
+// import authRouter from "./routes/routes.js";
+// import dbPool from "./db/connection.js";
 
 const app = express();
 
@@ -16,6 +16,6 @@ app.use(express.json());
 app.use("/api/v1", authRouter);
 
 app.listen(3000, () => {
-  dbPool.connect().then(console.log("Database connection successfull"));
+  // dbPool.connect().then(console.log("Database connection successfull"));
   console.log("Server is running on port 3000");
 });
